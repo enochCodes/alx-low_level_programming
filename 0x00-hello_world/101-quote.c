d03c176cd8b8a5e1fb9c3a8c86c416577474e7de
henok <henoksamson57@gmail.com>
2023-09-11 15:41:58 -0700

    UNIX Advance task fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - print "and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error."
+ * main - print "and that piece of art is useful" - Dora Korpar, 2015-10-19,"
  * Return: 1
  */
 int main(void)
