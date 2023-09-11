88112b01a811123f60401cde0a5af854acece50a
henok <henoksamson57@gmail.com>
2023-09-11 14:03:10 -0700

    c program file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry poin
- * return: Always 0(success)
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line
+ *
+ * Return: 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+
+	return (0);
 }
