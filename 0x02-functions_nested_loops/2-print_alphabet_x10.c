1ded553dd855f0971562d5a3bf86484e88da49b3
JANBAX12 <henoksamson57@gmail.com>
2023-09-15 12:55:07 +0000

    add loop nested loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -  prints 10 times the alphabet, in lowercase.
+ *
+ * Return: 0.
+ */
+void print_alphabet_x10(void)
+{
+	int loop;
+
+	for (loop = 0; loop <= 10; loop++)
+	{
+		int letter;
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar(10);
+	}
+}
