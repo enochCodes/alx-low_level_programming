369850ca9b51a0e43f17305f16f9c3c69f7b723b
JANBAX12 <henoksamson57@gmail.com>
2023-09-16 14:23:47 +0000

    fix the error

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,14 +7,13 @@
  * Return: void
 */
 
-void print_numbers(void) {
-
-  int number;
-
-  for (number = 48; number < 58; number++) {
-    _putchar(number);
-  }
-
-  _putchar('\n');
-
+void print_numbers(void){
+	
+	int number = 48;
+	
+	for (number = 48; number <= 58; number++)
+	{
+		_putchar(number);
+	}
+	_putchar(10);
 }
