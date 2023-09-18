e83dc610143425c023dc4cf58dda67f5d1270cff
JANBAX12 <henoksamson57@gmail.com>
2023-09-18 23:56:39 +0000

    change the position of the variables

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - a pointer to an int as parameter and updates
+ * @n: variable
+ * Return: 0.
+ */
+void reset_to_98(int *n)
+{
+	int a;
+
+	a = 98;
+	*n = a;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,5 +3,6 @@
 
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void reset_to_98(int *n);
 
 #endif
