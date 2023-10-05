00c0576e167290b541d0fc4b2a3136e7fb8f84b4
enochCodes <henoksamson57@gmail.com>
2023-10-05 14:13:37 +0000

    allocat memory

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *create_array - creates an array of chars,
+ * @c: charachtor
+ * @size: integer the size of the string
+ * return:  NULL if size = 0, and Returns a pointer to the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *A = malloc(size * sizeof(char));
+
+	if (A == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		unsigned int i;
+
+		for (i = 0; i < size; i++)
+		{
+			A[i] = c;
+		}
+		return (A);
+	}
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,6 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+char **strtow(char *str);
+char *create_array(unsigned int size, char c);
+
+#endif
