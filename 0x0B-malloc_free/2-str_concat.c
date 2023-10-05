cac2f4e9045f86e4bd32b21fbe3dfc1a967f9a55
enochCodes <henoksamson57@gmail.com>
2023-10-05 21:50:55 +0000

    remoive unused declaration

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * *str_concat - concatenates two strings.
+ * @s1: string one
+ * @s2: string two
+ * Return: returned pointer should point.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	if (s1 == NULL && s2 == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		int length1 = strlen(s1);
+		int length2 = strlen(s2);
+
+		char *str = malloc(sizeof(char) * length1 + length2 + 1);
+
+		if (str == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			strcpy(str, s1);
+			strcat(str, s2);
+		}
+		return (str);
+	}
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,5 +3,5 @@
 char **strtow(char *str);
 char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
-
+char *str_concat(char *s1, char *s2);
 #endif
