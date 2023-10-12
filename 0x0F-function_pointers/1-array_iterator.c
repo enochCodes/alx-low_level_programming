79f4b8675cd8c96f19673a5e1da69b5e36080775
enochCodes <henoksamson57@gmail.com>
2023-10-12 10:26:20 +0000

    fix in the heder file  double inclusion

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,9 +9,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
-	for (i = 0; i < size; i++)
+	if (array == NULL || action == NULL)
+		return;
+	while (size-- > 0)
 	{
-		(*action)(array[i]);
+		action(*array);
+		array++;
 	}
 }
