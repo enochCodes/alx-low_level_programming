864bb3c26a3301152716f819143f3b2a860562a9
enochCodes <henoksamson57@gmail.com>
2023-10-25 00:16:23 +0000

    ,fix the code

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,24 @@
 #include "lists.h"
+
 /**
- * print_listint - prints all the elements of a listint_t list
- * @h: constant pointer type list_t
+ * print_listint - prints all elements of listint_t list
+ * @h: head node
  *
- * Return: the numbers of nodes
+ * Return: no_nodes (no. of nodes)
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes;
+	/* stores no. of nodes */
+	size_t no_nodes = 0;
 
-	nodes = 0;
-
-	while (h)
+	/* traverse through all the elements of list */
+	while (h != NULL)
 	{
+		/* print element n */
 		printf("%d\n", h->n);
-		nodes++;
+		/* move to the next pointer */
 		h = h->next;
+		no_nodes++;
 	}
-	return (nodes);
+	return (no_nodes);
 }
--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,25 +1,26 @@
 #include "lists.h"
-
 /**
-*pop_listint - deletes the head node of a linked list
-*@head: head of the list
-*
-*Return: the deleted node data
-*/
-
+ * pop_listint - deletes the head node of a listint_t linked list
+ * @head: double pointer to the head of the list
+ *
+ * Return: the head nodes data (n)
+ */
 int pop_listint(listint_t **head)
 {
-listint_t *popped;
-int content;
-
-if (*head == NULL)
-return (0);
-
-popped = *head;
-content = popped->n;
-free(popped);
-
-*head = (*head)->next;
-return (content);
-
+	int n = 0;
+	listint_t *ptraux;
+	/* task condition */
+	if (*head == NULL)
+		return (0);
+	/* n is equal to the head de reference to n from the structure */
+	/* that is the nodes data (n) that are integers */
+	n = (*head)->n;
+	/* head goes to the next node */
+	ptraux = (*head)->next;
+	/* frees head so the head node is deleted */
+	free(*head);
+	/* now ptraux is the new head node */
+	*head = ptraux;
+	/* return the head nodes data that is n according to the structure */
+	return (n);
 }
