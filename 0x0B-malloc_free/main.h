25c40fa2636446f6879ccb2859cdcabc3d9da855
enochCodes <henoksamson57@gmail.com>
2023-11-05 11:13:21 +0000

    ubdate the repostory

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,4 +4,5 @@ char **strtow(char *str);
 char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
 char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
 #endif
