25402ff221aa1a29bd4c7e4ab8b0d3bba20a8adc
enochCodes <henoksamson57@gmail.com>
2023-12-11 14:58:50 +0300

    all

---
+++ 0x18-dynamic_libraries/c_files/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * *_strpbrk - searches a string for any of a set of bytes.
+ * @s: pointer to string
+ * @accept: pointer to strings
+ * Return: s that matches one of the bytes in accept, or NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *a;
+
+	while (*s != '\0')
+	{
+		for (a = accept; *a != '\0'; a++)
+		{
+			if (*s == *a)
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (NULL);
+}
