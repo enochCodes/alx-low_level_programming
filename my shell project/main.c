9ac759d0042aea799b3612143feb00ae8e1485ac
enochCodes <henoksamson57@gmail.com>
2023-12-11 17:52:37 +0300

    fix

---
+++ my shell project/main.c
@@ -0,0 +1,9 @@
+#include "shell.h"
+/**
+ * prompt_interpreter - prompt fuction
+ */
+int main() 
+{
+    prompt_interpreter();
+    return 0;
+}
---
+++ my shell project/shell.c
@@ -0,0 +1,10 @@
+#include "shell.h"
+/**
+ *  prompt_interpreter - Display a prompt-
+ *  and wait for the user to type a command.
+ */
+int main()
+{
+	prompt_interpreter();
+	return (0);
+}
