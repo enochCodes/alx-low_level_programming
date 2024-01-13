8d86c65f23e2fb5d10e373415ea236dd144e6b8b
enochCodes <henoksamson57@gmail.com>
2024-01-13 21:38:11 +0300

    fix_error

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main (int argc, char *argv[])
+/**
+ * main - print the number of arguments
+ * @argc: integer representing the number of arguments
+ * @argv: array of strings representing the arguments
+ * Return: always 0.
+ */
+int main(int argc, char *argv[])
 {
 	if (argc > 1 && argc < 3)
 	{
@@ -10,11 +15,12 @@ int main (int argc, char *argv[])
 
 		for (i = 1; i < argc; i++)
 		{
-			mul *= atoi(argv[i]);	
+			mul *= atoi(argv[i]);
 		}
 		printf("%d\n", mul);
 	}
-	else{
+	else
+	{
 		printf("Error\n");
 	}
 	return (0);
