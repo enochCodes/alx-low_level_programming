f7d0e25025d62ffd64813c18b117b4e33f8c1478
enochCodes <henoksamson57@gmail.com>
2024-01-13 20:20:15 +0300

    add

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
  * main - pass argumants
+ * @argc: integr to cunte the argumment size
+ * @argv: pointer arrray to store the arguments
  * Return: 0.
  */
 int main(int argc, char *argv[])
@@ -11,3 +13,4 @@ int main(int argc, char *argv[])
 	}
 	return (0);
 }
+
