509a51a465ef852e6604d8ba182fc2154727c7b9
enochCodes <henoksamson57@gmail.com>
2024-01-13 21:54:56 +0300

    final

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,8 +4,6 @@
  * main - print the number of arguments
  * @argc: integer representing the number of arguments
  * @argv: array of strings representing the arguments
- * @num1: integer
- * @num2: integer2
  * Return: always 0.
  */
 #include <stdio.h>
@@ -14,18 +12,19 @@
 int main(int argc, char *argv[])
 {
 	int num1, num2, result;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return 1;
+		return (1);
 	}
-	
+
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
-	
+
 	result = num1 * num2;
-	
+
 	printf("%d\n", result);
-	
-	return 0;
+
+	return (0);
 }
