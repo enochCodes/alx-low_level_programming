c88d10356d9f185b1b85ac5a0347ad5b17c4e08e
enochCodes <henoksamson57@gmail.com>
2024-01-17 00:37:11 +0300

    fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,8 +12,11 @@ char *str_concat(char *s1, char *s2)
 	int size;
 	char *concStr;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 
 	size = strlen(s1) + strlen(s2);
 
