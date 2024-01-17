40ba3854a6c04f0f60afa497009e66e51061aa5d
enochCodes <henoksamson57@gmail.com>
2024-01-17 21:56:52 +0300

    done

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * countSpaces - count the word
+ * @str: string
+ * Return: numebr of words
+ */
+int countSpaces(const char *str)
+{
+	int count;
+
+	count = 0;
+
+	while (*str)
+	{
+		if (*str == ' ')
+		{
+			count++;
+		}
+		str++;
+	}
+	return (count);
+}
+#include "main.h"
+/**
+ *  **strtow - count words
+ *  @str: string
+ *  Return: return the word
+ */
+char **strtow(char *str)
+{
+	int wordCount = countSpaces(str) + 1;
+	char **words = (char **)malloc(wordCount * sizeof(char *));
+	int i;
+	int j;
+	int wordLength;
+	const char *wordStart;
+
+	if (words == NULL)
+	{
+		return (NULL);
+	}
+
+	i = 0;
+
+	while (*str)
+	{
+		while (*str == ' ')
+		{
+			str++;
+		}
+		if (*str == '\0')
+		{
+			break;
+		}
+		wordStart = str;
+		while (*str && *str != ' ')
+		{
+			str++;
+		}
+		wordLength = str - wordStart;
+		words[i] = (char *)malloc((wordLength + 1) * sizeof(char));
+		if (words[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(words[j]);
+			}
+			free(words);
+			return (NULL);
+		}
+		strncpy(words[i], wordStart, wordLength);
+		words[i][wordLength] = '\0';
+		i++;
+	}
+	return (words);
+}
