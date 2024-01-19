633d4da82dd4d7a47d01e8d4a3cfe4ee4c43c8d7
enochCodes <henoksamson57@gmail.com>
2024-01-19 23:31:44 +0300

    fix

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * *_calloc - total
+ * @nmwnb: located
+ * @ptr: nothing
+ * @return: build same
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	size_t total_size;
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	total_size = nmemb * size;
+
+	ptr = malloc(total_size);
+	
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+    
+
+	memset(ptr, 0, total_size);
+	
+	return (ptr);
+}
