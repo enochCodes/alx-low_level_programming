0ba2f7000af177c64d0407799e9907ba8819d217
enochCodes <henoksamson57@gmail.com>
2024-01-24 05:16:05 +0300

    done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef _DOG_H
 #define _DOG_H
-
+/**
+ * struct dog - a data srtuct for the dog
+ * @name: pointer to charactor
+ * @age: pointer to integer
+ * @owner: pominter to charactor
+ */
 struct dog
 {
 	char *name;
