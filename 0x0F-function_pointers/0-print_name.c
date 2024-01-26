ccd94c00e3c9fec4e8ba346033087e5b8d228fe6
enochCodes <henoksamson57@gmail.com>
2024-01-27 00:29:58 +0300

    done_first

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - print the name
+ * @name: charactor
+ * @f: functional pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,4 +2,6 @@
 #define _FUNCTION_POINTERS_
 
 int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
 #endif
