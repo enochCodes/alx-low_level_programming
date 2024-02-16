42ec86141ac21ef7fb8ebaea2408bcd6803b16e4
enochCodes <henoksamson57@gmail.com>
2024-02-16 20:53:03 +0300

    done

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,15 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdlib.h>
-
+#include <string.h>
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
