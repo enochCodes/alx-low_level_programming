7ee6157b465a4ff14e884253fc41ffa4076d05de
enochCodes <henoksamson57@gmail.com>
2024-04-18 00:44:23 +0300

    dobne

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,6 +1,9 @@
 #ifndef HASH_TABLE_
 #define HASH_TABLE_
 
+#include <string.h>
+
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -31,4 +34,10 @@ typedef struct hash_table_s
 
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
+shash_table_t *shash_table_create(unsigned long int size);
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+char *shash_table_get(const shash_table_t *ht, const char *key);
+void shash_table_print(const shash_table_t *ht);
+void shash_table_print_rev(const shash_table_t *ht);
+void shash_table_delete(shash_table_t *ht);
 #endif
