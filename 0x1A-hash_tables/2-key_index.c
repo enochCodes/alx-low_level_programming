7ecca5d142dee295a9ef9feefe6050e8c121ac62
enochCodes <henoksamson57@gmail.com>
2024-05-05 09:22:18 +0300

    done the betty error

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,9 +1,11 @@
 #include "hash_tables.h"
 /**
  * key_index - function retur the hash
+ * @key: the key for the hash
+ * @size: size of the hash table
  * Return: function return
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    return (hash_djb2(key) % size);
+	return (hash_djb2(key) % size);
 }
